a38ab1602e036f876878666748c3dbe37e044777
Oscar Quintanilla <38816404+owqh@users.noreply.github.com>
2019-11-12 00:25:27 -0600

    creacion de incio de crud
    prueba inicial de crud

---
+++ ClinicaMedica/ClinicaMedica/Models/Paciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicaMedica.Models
+{
+    public class Paciente
+    {
+        public int codigo_paciente { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public int dui { get; set; }
+        public string fecha_nacimiento { get; set; }
+        public char sexo { get; set; }
+        public string correo { get; set; }
+        public string estado_civil { get; set; }
+        public int peso { get; set; }
+        public int altura { get; set; }
+        public int telefono { get; set; }
+        public string alergias { get; set; }
+        public string padecimieto_previo { get; set; }
+    }
+}
